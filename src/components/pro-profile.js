269e280d64806079ff4ce95b359d1ea1109a8c42
Enzo Aliatis <enzo.aliatis@gmail.com>
2020-01-07 10:36:34 -0500

    init listo

---
+++ src/components/pro-profile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const ProProfile = () => (
+  <section className='hero is-medium is-primary'>
+    <div className='hero-body'>
+      <div className='container'>
+        <h1 className='title'>Aqui va mi nombre</h1>
+      </div>
+      <div className='container'>
+        <h1 className='subtitle'>Aqui va mi universidad</h1>
+      </div>
+    </div>
+  </section>
+)
+
+export default ProProfile
