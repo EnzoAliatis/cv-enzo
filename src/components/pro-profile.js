d54b15e7e29636fc07ac3ad33a085e15a3acc3a9
Enzo Aliatis <enzo.aliatis@gmail.com>
2020-01-09 22:28:31 -0500

    ya vengo

--- src/components/pro-profile.js
+++ src/components/pro-profile.js
@@ -30,32 +30,42 @@ const ProProfile = () => {
       Tag="div"
       fluid={background.childImageSharp.fluid}
       backgroundColor={`#040e18`}
-      style={{ width: '100%', backgroundSize: 'cover', color: 'white' }}
+      style={{ width: "100%", backgroundSize: "cover", color: "white" }}
     >
       <section className="hero">
         <div className="hero-body">
-          <div className="container profileContentBox">
-            <figure className="image profileImageBox">
-              <ProfileImage />
-            </figure>
-            <div>
-              <h1 className="title">{photo.siteMetadata.title}</h1>
-              <h2 className="subtitle" style={{ marginBottom: 0 }}>
-                {photo.siteMetadata.jobProfile}
-              </h2>
-              <h6 className="subtitle is-6">{photo.siteMetadata.email}</h6>
+          <div className="columns is-centered is-multiline">
+            <div className="column is-three-quarters">
+              <div className="columns">
+                <div className="column is-one-fifth">
+                  <figure className="image profileImageBox">
+                    <ProfileImage />
+                  </figure>
+                </div>
+                <div className="column">
+                  <h1 className="title">{photo.siteMetadata.title}</h1>
+                  <h2 className="subtitle" style={{ marginBottom: 0 }}>
+                    {photo.siteMetadata.jobProfile}
+                  </h2>
+                  <h6 className="subtitle is-6">{photo.siteMetadata.email}</h6>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="container profileSubContentBox">
-            <figure className="image uleamImageBox">
-              <UleamLogoImage />
-            </figure>
-            <div className="content" style={{ maxWidth: 401 }}>
-              <p>
-                I'm in last year of engineer in computational system. <br />
-                Passionate about the code and problem solver with technology as
-                the main resource. Programming since 2016
-              </p>
+            <div className="column is-three-quarters">
+              <div className="columns">
+                <div className="column">
+                  <figure className="image uleamImageBox">
+                    <UleamLogoImage />
+                  </figure>
+                </div>
+                <div className="column">
+                  <p>
+                    I'm in last year of engineer in computational system. <br />
+                    Passionate about the code and problem solver with technology
+                    as the main resource. Programming since 2016
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
