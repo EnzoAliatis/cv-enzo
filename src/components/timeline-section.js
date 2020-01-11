eb57c9cffc98c8974ba5c15b36a523f6636ce943
Enzo Aliatis <enzo.aliatis@gmail.com>
2020-01-11 11:43:17 -0500

    listo el timeline section. pasemos al proximo

--- src/components/timeline-section.js
+++ src/components/timeline-section.js
@@ -8,7 +8,7 @@ const TimelineSection = () => {
           <div className="columns is-centered">
             <div className="column is-three-quarters-desktop">
               <h2 className="title is-2 has-text-centered">
-                Experience overview
+                Overview experience
               </h2>
             </div>
           </div>
@@ -21,11 +21,9 @@ const TimelineSection = () => {
                       <div className="circle is-content-centered">2016</div>
                     </div>
                     <div className="column is-6">
-                      <h5>Fully Responsive</h5>
+                      <h5 className='title is-5 is-marginless'>The journey start</h5>
                       <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed porttitor gravida aliquam. Morbi orci urna, iaculis
-                        in ligula et, posuere interdum lectus.
+                        When I was 20, I found my passion, the code, and since then I started learning about programming for never stop. I started watching videos at youtube, platzi, egghead, udemy and reading books. Since then I solve all my problems using technology.
                       </p>
                     </div>
                   </div>
@@ -42,11 +40,9 @@ const TimelineSection = () => {
                   </div>
                   <div className="columns is-mobile align-items-center justify-content-end how-it-works">
                     <div className="column is-6 has-text-right">
-                      <h5>Using Bootstrap</h5>
+                      <h5 className='title is-5 is-marginless'>Travesia Hotel</h5>
                       <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed porttitor gravida aliquam. Morbi orci urna, iaculis
-                        in ligula et, posuere interdum lectus.
+                        I worked in a family restaurant-hotel. We needed to invoice quickly and take control of what was sold, at first, we used a pencil and book, I developed a point of sale that helped us started working quickly and adequately.
                       </p>
                     </div>
                     <div className="column is-2  has-text-centered full">
@@ -69,11 +65,9 @@ const TimelineSection = () => {
                       <div className="circle is-content-centered">2019</div>
                     </div>
                     <div className="column is-6">
-                      <h5>Now with Pug and Sass</h5>
+                      <h5 className='title is-5 is-marginless'>La Fabril</h5>
                       <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed porttitor gravida aliquam. Morbi orci urna, iaculis
-                        in ligula et, posuere interdum lectus.
+                        I worked in one of the most important companies in my country, there I worked as a frontend developer, developing many web interfaces.
                       </p>
                     </div>
                   </div>
@@ -82,11 +76,9 @@ const TimelineSection = () => {
                       <div className="circle is-content-centered">2019</div>
                     </div>
                     <div className="column is-6">
-                      <h5>Fully Responsive</h5>
+                      <h5 className='title is-5 is-marginless'>Tus10Segundos</h5>
                       <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed porttitor gravida aliquam. Morbi orci urna, iaculis
-                        in ligula et, posuere interdum lectus.
+                        I was lucky to work, remotely, with a Mexican YouTuber, we developed a web platform for the sale of their services
                       </p>
                     </div>
                   </div>
@@ -103,11 +95,9 @@ const TimelineSection = () => {
                   </div>
                   <div className="columns is-mobile align-items-center justify-content-end how-it-works mb-0">
                     <div className="column is-6 has-text-right">
-                      <h5>Using Bootstrap</h5>
+                      <h5 className='title is-5 is-marginless'>Always improving</h5>
                       <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed porttitor gravida aliquam. Morbi orci urna, iaculis
-                        in ligula et, posuere interdum lectus.
+                        More than 4 years in the environment, more than 40 approved courses, I am ready to continue my jornie as a developer and continue solving problems using technology and always learning how to be a better programmer.
                       </p>
                     </div>
                     <div className="column is-2  has-text-centered full">
