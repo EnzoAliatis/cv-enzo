96fd86233d43e300c611e4eea0f19a4924fb9f1e
Enzo Aliatis <enzo.aliatis@gmail.com>
2020-01-13 12:30:27 -0500

    ready for deployment

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,6 @@ import React from "react"
 import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa"
 import { MdEmail } from "react-icons/md"
 
-
 const Footer = () => {
   return (
     <footer className="footer myfooter">
@@ -10,23 +9,61 @@ const Footer = () => {
         <h3 className="subtitle is-3" style={{ color: "#fff" }}>
           Contacts
         </h3>
-        <div className="column is-three-quarters" style={{margin: 'auto'}}>
+        <div className="column is-three-quarters" style={{ margin: "auto" }}>
           <div className="columns is-centered">
             <div className="column has-text-centered is-content-centered">
-              <FaGithub size={28} color='#fff'/> <span style={{marginLeft: 5, color:'#fff' }}>EnzoAliatis</span>
+              <a
+                href="https://github.com/EnzoAliatis"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="is-content-centered"
+              >
+                <FaGithub size={28} color="#fff" />{" "}
+                <span style={{ marginLeft: 5, color: "#fff" }}>
+                  EnzoAliatis
+                </span>
+              </a>
+            </div>
+            <div className="column has-text-centered">
+              <a
+                href="https://twitter.com/enzoa23"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="is-content-centered"
+              >
+                <FaTwitter size={28} color="#00acee" />{" "}
+                <span style={{ marginLeft: 5, color: "#00acee" }}>@enzo23</span>
+              </a>
             </div>
             <div className="column has-text-centered is-content-centered">
-              <FaTwitter size={28} color='#00acee'/> <span style={{marginLeft: 5, color:'#00acee'}}>@enzo23</span>
+              <a
+                href="https://www.linkedin.com/in/enzo-aliatis/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="is-content-centered"
+              >
+                <FaLinkedin size={28} color="#0e76a8" />{" "}
+                <span style={{ marginLeft: 5, color: "#0e76a8" }}>
+                  enzo-aliatis
+                </span>
+              </a>
             </div>
             <div className="column has-text-centered is-content-centered">
-              <FaLinkedin size={28} color='#0e76a8'/> <span style={{marginLeft: 5, color:'#0e76a8'}}>enzo-aliatis</span>
-            </div>
-            <div className="column has-text-centered is-content-centered">
-              <MdEmail size={28} color='#fff' /> <span style={{marginLeft: 5, color:'#fff'}}>enzo.aliatis@gmail.com</span>
+              <a
+                href="mailto:enzo.aliatis@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="is-content-centered"
+              >
+                <MdEmail size={28} color="#fff" />{" "}
+                <span style={{ marginLeft: 5, color: "#fff" }}>
+                  enzo.aliatis@gmail.com
+                </span>
+              </a>
             </div>
           </div>
-          <div className="column" style={{margin:'auto'}}>
-            <h4 className='is-5 has-text-grey-dark'>© 2016-2020</h4>
+          <div className="column" style={{ margin: "auto" }}>
+            <small className="has-text-grey-dark">© 2020-2021</small>
           </div>
         </div>
       </div>
